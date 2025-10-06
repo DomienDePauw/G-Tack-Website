9a85ef3ad389d9f40454b7621698294ef2481b0b
Domien De Pauw <domiendepauw@hotmail.com>
2025-10-07 01:20:38 +0200

    IntroSection component - custom-font added

--- Tack/src/App.jsx
+++ Tack/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import './App.css'
+import IntroSection from './components/IntroSection'
 
 function App() {
   return (
-    <div>
-      <AboutSection />
-    </div>
+      <IntroSection/>
   )
 }
 
