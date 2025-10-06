1c0debe81bbfb6442c02861b416964174e7c444b
Domien De Pauw <domiendepauw@hotmail.com>
2025-10-07 00:23:11 +0200

    components

---
+++ Tack/src/components/AboutSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutSection = () => {
+  
+  return (
+    <section
+      id="about"
+      className="about-section bg-gray-100 py-12 px-4 sm:px-6 lg:px-8"
+    />
+
+  );
+};
+
+export default AboutSection;
