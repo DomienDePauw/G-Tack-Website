3719d53dfdceb61e6beb8ed4969303014bc31da3
Domien De Pauw <domiendepauw@hotmail.com>
2025-10-07 02:13:44 +0200

    Introsection comp - slider - images

--- Tack/src/components/IntroSection.jsx
+++ Tack/src/components/IntroSection.jsx
@@ -4,7 +4,7 @@ import image2 from "../assets/images/thumbnail_IMG_9489.jpg";
 import image3 from "../assets/images/thumbnail_IMG_9368.jpg";
 
 const IntroSection = () => {
-  const images = [image1, image2, image3];
+  const images = [image1,image2,image3];
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
@@ -17,22 +17,22 @@ const IntroSection = () => {
   return (
     <section
       id="intro"
-      className="flex flex-col md:flex-row items-center justify-between px-8 md:px-16 py-20"
+      className="flex flex-row items-center px-8 py-20"
     >
-      <div className="md:w-1/2 mb-10 md:mb-0 md:pr-10">
-        <h2 className="text-7xl font-synemono mb-6 text-black-600">
+      <div className="w-1/2">
+        <h2 className="text-7xl font-synemono mb-10 px-8 text-black-600">
           Tack Goretti
         </h2>
-        <p className="text-2xl text-black-200 font-synemono mb-4">
+        <p className="text-2xl text-black-200 px-8 font-synemono">
           (H)eerlijke lokale creatieve catering op maat gebracht, we willen bijdragen aan een gezonde en duurzame toekomst.
         </p>
       </div>
       
-      <div className="relative md:w-1/2 flex items-center justify-center">
+      <div className="w-1/2 flex px-8 items-center justify-center">
         <img
           src={images[current]}
           alt={`Slide ${current + 1}`}
-          className="rounded-2xl shadow-2xl w-full h-80 object-cover transition-all duration-700 ease-in-out"
+          className="rounded-2xl shadow-2xl w-full h-80 object-cover"
         />
       </div>
     </section>
