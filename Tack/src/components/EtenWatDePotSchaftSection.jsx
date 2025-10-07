53547b662f482f1bbb6e30a46c5eefd7a989e417
Domien De Pauw <domiendepauw@hotmail.com>
2025-10-07 13:31:51 +0200

    +

--- Tack/src/components/EtenWatDePotSchaftSection.jsx
+++ Tack/src/components/EtenWatDePotSchaftSection.jsx
@@ -3,7 +3,16 @@ import image1 from '../assets/images/EtenWatDePotSchaft.png'
 
 export const EtenWatDePotSchaftSection = () => {
   return (
-    <div>EtenWatDePotSchaftSection</div>
+    <section>
+        <div className='mt-12 mb-12 flex justify-center'>
+            <img
+                src={image1}
+                alt='EtenWatDePotSchaft'
+                className=''
+            >
+            </img>
+        </div>
+    </section>
   )
 }
 
